8b2b95c49a212295c4481b61b8f08c541f7f286e
Idam <i.faizin@cebes.co.id>
2024-05-06 14:23:27 +0700

    fix corsMiddleware

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -2,7 +2,7 @@ import secure from 'cors';
 
 export function cors() {
 	return secure({
-		origin: process.env.NODE_ENV ? `${process.env.BASE_URL}` : true,
+		origin: process.env.NODE_ENV ? `${process.env.APP_URL}` : true,
 		credentials: true,
 	});
 }
